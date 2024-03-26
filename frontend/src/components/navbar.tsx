953168193c25ccbe5f3d0e7f91f31979ab2a1368
utan323 <nglephuc03@gmail.com>
2024-03-26 07:02:35 +0700

    initialize frontend

---
+++ frontend/src/components/navbar.tsx
@@ -0,0 +1,11 @@
+
+
+const Navbar = () => {
+    return (
+        <div className="h-screens w-1/4 bg-white">
+            
+        </div>
+    )
+};
+
+export default Navbar;
---
+++ frontend/src/views/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/navbar";
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <h1>Hello world! {encodeURI(":))")}</h1>
+      <h1>Edit views/App.tsx to begin!</h1>
+    </>
+  );
+};
+
+export default App;
