20e2735f68f6b209651cc92b56c16b4cdadd52ea
Example113355 <tson113355@gmail.com>
2024-04-03 00:37:15 +0700

    UI of environment page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./views/layout.tsx";
 import ControlBoard from "./components/controlBoard.tsx";
+import Environment from "./components/environmentScreen.tsx";
 
 const App = () => {
     return (
@@ -10,7 +11,7 @@ const App = () => {
                     <Route path="/signup" element={<div>Hello world from sign up</div>} />
                     <Route path="/signin" element={<div>Hello world from sign in</div>} />
                     <Route path="/" element={<Layout />}>
-                        <Route path="home" element={<ControlBoard />} />
+                        <Route path="home" element={<Environment />} />
                         <Route path="home1" element={<div>Home 2</div>} />
                         <Route index element={<div>Hello world</div>} />
                     </Route>
--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -11,11 +11,27 @@ const Header = (props: HeaderProps): ReactNode => {
         <>
             <div className="w-full h-[10%] px-5 flex items-center bg-white border-b-2 border-gray-300">
                 {
-                    props.isMobile ? <span onClick={() => { props.handleNavbar() }}>Open</span> : <span>a</span>
+                    props.isMobile ? <span onClick={() => { props.handleNavbar() }}>Open</span> : <Navbar />
                 }
             </div>
         </>
     )
 };
+
+function Navbar() {
+    return (
+        <>
+            <button className="text-2xl text-cyan-600 mr-20">YOLOHOME</button>
+
+            <div className="">
+                <div className="">
+                    <span className="text-lg mr-40 text-black">Environment</span>
+                    <input type="text" placeholder="Search..."  className="border rounded-lg p-1.5"/>
+                </div>
+
+            </div>
+        </>
+    )
+}
 
 export default Header;
