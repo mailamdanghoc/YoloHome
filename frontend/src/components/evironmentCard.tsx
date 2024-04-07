aca74ecc8fc02f310a5fc68a75d8ca9c199ca9d6
Example113355 <tson113355@gmail.com>
2024-04-07 17:35:34 +0700

    feature:add Chart

--- frontend/src/components/evironmentCard.tsx
+++ frontend/src/components/evironmentCard.tsx
@@ -2,30 +2,38 @@ import { ReactNode } from 'react';
 import TempIcon from './icons/tempIcon';
 import HumidIcon from './icons/humidIcon';
 import LightIcon from './icons/lightIcon';
+import {CircularProgressbar} from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 interface EnvCardProps {
     name: string;
     color: string;
     className: string;
     icon: string;
+    curVal: number;
+    limit: number;
 }
 
-const EnvCard = ({ name, color, className, icon} : EnvCardProps): ReactNode => {
+const EnvCard = ({ name, color, className, icon, curVal, limit} : EnvCardProps): ReactNode => {
     const IconComponents : { [key: string]: any } = {
         temp: TempIcon,
         humid: HumidIcon,
         light: LightIcon,
     }
     const Icon = IconComponents[icon];
+    const percentage = curVal / limit * 100;
+    const unitStr = icon === "temp" ? "C" : "cd";
 
     return (
         <div className="bg-white w-2/5 rounded-2xl p-4 mt-4" >
             <div className="text-xl font-semibold">{name}</div>
             <div className="mt-2 flex items-center justify-around">
                 <Icon color={color} className={className}/>
-                <div className="bg-slate-400 w-14 h-14 rounded-full flex items-center justify-center">
-                    <span>22C</span>
+                <div className="w-14 h-14">
+                    <CircularProgressbar strokeWidth={12} value={percentage} text={`${percentage}${unitStr}`} />
                 </div>
+                {/* <div className="bg-slate-400 w-14 h-14 rounded-full flex items-center justify-center">
+                </div> */}
             </div>
         </div>
     )
