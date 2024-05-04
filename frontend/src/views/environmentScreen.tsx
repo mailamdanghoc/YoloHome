fb13c35f33db35bb8fe36f1249e64cb5a5d16c09
Example113355 <tson113355@gmail.com>
2024-05-04 17:01:12 +0700

    feature: add threshold

--- frontend/src/views/environmentScreen.tsx
+++ frontend/src/views/environmentScreen.tsx
@@ -1,6 +1,5 @@
 import { ReactNode, useEffect, useState } from 'react';
 import EnvCard from '../components/evironmentCard';
-import BarChart from '../components/BarChart';
 import useSocket from '../customizes/useSocket';
 import axios from 'axios';
 import { useOutletContext } from 'react-router-dom';
@@ -8,11 +7,30 @@ import LineChart from '../components/LineChart';
 
 const data1 = [ 0 ];
 
+const humidUrl = "http://localhost:3001/api/v1/devices/660ebbdbb55dfa6d3aa6ab9d"
+const lightUrl = "http://localhost:3001/api/v1/devices/660ebc78b55dfa6d3aa6ab9f"
+
+const fetchThreshold = async (url : string) => {
+    try {
+        const res = await axios.get(url);
+        return Number(res.data.threshold);
+    }
+    catch (err) {
+        console.log(err);
+        return 0;
+    }
+}
+
+const curHumidLim = await fetchThreshold(humidUrl);
+const curLightLim = await fetchThreshold(lightUrl);
+
 const Environment = (props: any): ReactNode => {
     const [light, setLight] = useState(0);
     const [humid, setHumid] = useState(0);
     const socket = useSocket;
     const [graphData, setGraphData] = useState(data1);
+    const [humidLimit, setHumidLimit] = useState(curHumidLim);
+    const [lightLimit, setLightLimit] = useState(curLightLim);
     const handleSetTitle = useOutletContext<(title: string) => void>();
 
     let temperature = 0;
@@ -57,8 +75,8 @@ const Environment = (props: any): ReactNode => {
             </div>
             <span className='text-2xl font-bold'>Current environment information</span>
             <div className="mt-4 flex justify-around flex-wrap">
-                <EnvCard curVal={light} limit={100} name="Light" color="#00b3a7" className="w-14" icon="light"/>
-                <EnvCard curVal={humid} limit={100} name="Humidity" color="#00b3a7" className="w-14" icon="humid"/>
+                <EnvCard curVal={light} limit={lightLimit} setLimit={setLightLimit} name="Light" color="#00b3a7" className="w-14" icon="light"/>
+                <EnvCard curVal={humid} limit={humidLimit} setLimit={setHumidLimit} name="Humidity" color="#00b3a7" className="w-14" icon="humid"/>
             </div>
         </div>
     )
